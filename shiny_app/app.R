b46581937d41838bd4f34ee99ad80ac7d24e0871
kaitlynhair <kaitlyn.hair@ed.ac.uk>
2024-12-18 12:42:31 +0000

    add shiny app

---
+++ shiny_app/app.R
@@ -0,0 +1,180 @@
+require(shiny)
+require(readr)
+require(DT)
+library(shinyhelper)
+require(stringr)
+require(dplyr)
+library(shinyalert)
+library(progressr)
+library(networkD3)
+library(rsconnect)
+library(RCurl)
+library(shiny)
+library(ASySD)
+library(shinythemes)
+library(knitr)
+library(shinycssloaders)
+library(htmlwidgets)
+library(shinyWidgets)
+library(bslib)
+
+# Define UI for application
+ui <- fluidPage(
+
+  # Application title
+  titlePanel("DREAMR"),
+
+  # Sidebar with file upload options
+  sidebarLayout(
+    sidebarPanel(
+      h4("Upload citations file ", icon("upload")),
+
+      shinyWidgets::prettyRadioButtons(
+        inputId = "fileType",
+        label = "Choose a file type to upload",
+        inline = TRUE,
+        choices = c("Endnote XML",
+                    "CSV", "BIB", "RIS", "Zotero CSV", "Tab delimited"),
+        status = "primary"),
+      br(),
+
+      # Input: select a file to upload
+      fileInput("uploadfile", "Choose file(s) to upload",
+                multiple = TRUE,
+                placeholder = "No file selected")
+    ),
+
+    # Main panel with tabs
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Summary of Uploaded Studies",
+                 br(),
+                 h4("Citation Summary"),
+                 tableOutput("summaryTable"),
+                 br(),
+                 h4("Percentage of DOI and PMID Presence"),
+                 tableOutput("percentageTable")
+        ),
+        tabPanel("Transparency",
+                 br(),
+                 h4("Author Distribution"),
+                 p("Author Distribution Visualization Placeholder") # Content placeholder
+        ),
+        tabPanel("Author Institutions",
+                 br(),
+                 h4("Author Distribution"),
+                 p("Author Distribution Visualization Placeholder") # Content placeholder
+        ),
+        tabPanel("Author Location",
+                 br(),
+                 h4("Author Distribution"),
+                 p("Author Distribution Visualization Placeholder") # Content placeholder
+        )
+      )
+    )
+  )
+)
+
+# Define server logic
+server <- function(input, output) {
+
+  # Reactive values for storage
+  rv <- shiny::reactiveValues()
+  rv$refdata <- NULL
+  rv$citation_summary <- data.frame()
+  rv$oa_data <- NULL
+
+  # File upload event
+  shiny::observeEvent(input$uploadfile, {
+    shiny::validate(need(input$uploadfile != "", "Select your citation file to upload..."))
+
+    if (is.null(input$uploadfile)) {
+      return(NULL)
+    } else {
+      isolate(
+        if(input$fileType == "Endnote XML" & all(grepl(".xml$", input$uploadfile$name))){
+          citations <- load_multi_search(input$uploadfile$datapath, input$uploadfile$name, method = "endnote")
+        } else if(input$fileType == "CSV" & all(grepl(".csv$", input$uploadfile$name))){
+          citations <- load_multi_search(input$uploadfile$datapath, input$uploadfile$name, method = "csv")
+        } else if(input$fileType == "Zotero CSV" & all(grepl(".csv$", input$uploadfile$name))){
+          citations <- load_multi_search(input$uploadfile$datapath, input$uploadfile$name, method = "zotero_csv")
+        } else if(input$fileType == "RIS" & all(grepl(".txt|.ris$", input$uploadfile$name))){
+          citations <- load_multi_search(input$uploadfile$datapath, input$uploadfile$name, method = "ris")
+        } else if(input$fileType == "BIB" & all(grepl(".bib$", input$uploadfile$name))){
+          citations <- load_multi_search(input$uploadfile$datapath, input$uploadfile$name, method = "bib")
+        } else if(input$fileType == "Tab delimited"){
+          citations <- load_multi_search(input$uploadfile$datapath, input$uploadfile$name, method = "txt")
+        } else {
+          shinyalert("Wrong file type selected",
+                     "The file extension doesn't match the selected upload format", type = "warning")
+          shinyjs::reset(id = "uploadfile", asis = FALSE)
+          return()
+        }
+      )
+
+      # Ensure unique record_id
+      if (length(unique(citations$record_id)) != nrow(citations)) {
+        shinyalert("Unique identifier generated!",
+                   "The record_id column within uploaded citations was not unique. ASySD has generated a unique record_id for each citation. You can preview this in the table.", type = "info")
+
+        citations <- citations %>%
+          mutate(record_id = as.character(row_number() + 1000))
+      }
+      rv$refdata <- citations
+
+      # Summary calculations
+      citation_summary <- rv$refdata %>%
+        select(source, label, file_name) %>%
+        group_by(file_name) %>%
+        add_count(name = "number_of_citations") %>%
+        group_by(file_name, number_of_citations) %>%
+        distinct() %>%
+        summarise(source = ifelse(all(is.na(source)), "", paste(ifelse(!is.na(source), source, NA), collapse = ", ")),
+                  label = ifelse(all(is.na(label)), "", paste(ifelse(!is.na(label), label, NA), collapse = ", "))) %>%
+        mutate(source = ifelse(str_length(source) > 10, "multiple", source),
+               label = ifelse(str_length(label) > 10, "multiple", label)) %>%
+        select(file_name, number_of_citations, label, source)
+
+      rv$citation_summary <- citation_summary
+
+      # Summary calculations
+      oa_data <- oa_metadata(rv$refdata, identifier="doi")
+      rv_oa_data_count <- length(oa_data$id)
+      rv$oa_data <- oa_data
+
+    }
+  })
+
+  # Output: Summary table
+  output$summaryTable <- renderTable({
+    rv$citation_summary
+    rv$oa_data_count
+  })
+
+  # Output: Percentage table (DOI and PMID presence)
+  output$percentageTable <- renderTable({
+    req(rv$refdata)
+    refdata <- rv$refdata
+
+    total_records <- nrow(refdata)
+
+    # Calculate DOI and PMID percentages
+    doi_present <- sum(!is.na(refdata$doi) & refdata$doi != "") / total_records * 100
+    pmid_present <- if ("pmid" %in% colnames(refdata)) {
+      sum(!is.na(refdata$pmid) & refdata$pmid != "") / total_records * 100
+    } else {
+      NA
+    }
+
+    percentage_table <- data.frame(
+      Metric = c("% DOI Present", "% PMID Present"),
+      Percentage = c(round(doi_present, 2), round(pmid_present, 2))
+    )
+
+    percentage_table
+  })
+
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
--- start.R
+++ start.R
@@ -1,17 +1,105 @@
 library(dplyr)
 source("R/oa_metadata.R")
 
-dat <- read_excel("data/shortcut_citations/dataset_all_fields_prevalence.xlsx")
-test <- dat[1:20,]
+dat <-readxl::read_excel("data/shortcut_citations/dataset_all_fields_prevalence.xlsx")
 
-test <- test %>%
+test <- dat %>%
   select(title, author, pubmed_id, url) %>%
   mutate(pmid = paste0("pmid:", pubmed_id))
 
 result <- oa_metadata(test, identifier="pmid")
 
+# how much publication language?
+language <- result %>% group_by(doi) %>% select(language, id) %>% unique()
+
 # get institutions
 institutions <- extract_institution(result)
+institutions_sliced <- institutions %>%
+  filter(!institution_id =="Unknown") %>%
+  group_by(doi) %>%
+  slice_head()
+length(unique(institutions_sliced$doi))
+
+save(authors, file="authors.Rdata")
 
 # get funders
 funders <- extract_funder(result)
+funders <- soles::format_doi(funders)
+funders_sliced <- funders %>%
+  filter(!funder_name =="Unknown") %>%
+  group_by(doi) %>%
+  slice_head()
+length(unique(funders_sliced$doi))
+
+
+# abstracts
+abstracts <- result %>%
+  select(id, ab) %>%
+  filter(!ab=="") %>%
+  unique()
+
+# sample of 10
+set.seed(101)
+ten <- dat[1:10,]
+random <- dat[sample(nrow(dat), 10), ]
+random <- random %>% select(title, author, year, url, pubmed_id)
+
+# After validation check
+check <- test %>% filter(pubmed_id %in% c("31599027","32131891", "32203578",
+                                     "31916270","31928188","31953872",
+                                     "32048161","31957074","31152389",
+                                     "31100369"))
+result_checked <- oa_metadata(check, identifier="pmid")
+result_checked <- result_checked %>%
+  select()
+
+# get institutions
+institutions_first_author <- extract_institution(result_checked, author_position = "first")
+institutions_first_author_sliced <- institutions_first_author %>%
+  filter(!institution_id =="Unknown") %>%
+  group_by(doi) %>%
+  slice_head() %>%
+  select(name, doi) %>%
+  rename(first_author_institution = name)
+
+# get institutions
+institutions_last_author <- extract_institution(result_checked, author_position = "last")
+institutions_last_author_sliced <- institutions_last_author %>%
+  filter(!institution_id =="Unknown") %>%
+  group_by(doi) %>%
+  slice_head() %>%
+  select(name, doi) %>%
+  rename(last_author_institution = name)
+
+funders <- extract_funder(result_checked)
+
+first_author_career_stage <- result_checked %>%
+  tidyr::unnest(author) %>%
+  select(id, counts_by_year, au_display_name, au_id, author_position) %>%
+  tidyr::unnest(counts_by_year) %>%
+  filter(author_position=="first") %>%
+  group_by(au_id) %>%
+  arrange(year) %>%
+  mutate(first_author_active = as.numeric(format(Sys.Date(), "%Y")) - first(year)) %>%
+  select(-year, -cited_by_count, -author_position) %>%
+  unique() %>%
+  ungroup()
+
+last_author_career_stage <- result_checked %>%
+  tidyr::unnest(author) %>%
+  select(id, counts_by_year, au_display_name, au_id, author_position) %>%
+  tidyr::unnest(counts_by_year) %>%
+  filter(author_position=="last") %>%
+  group_by(au_id) %>%
+  arrange(year) %>%
+  mutate(last_author_active = as.numeric(format(Sys.Date(), "%Y")) - first(year)) %>%
+  select(-year, -cited_by_count, -author_position, -authors, -au_id) %>%
+  unique() %>%
+  ungroup()
+
+result_checked_final <- result_checked %>%
+  select(title, doi, id) %>%
+  left_join(first_author_career_stage) %>%
+  left_join(last_author_career_stage) %>%
+  left_join(institutions_last_author) %>%
+  left_join(institutions_first_author)
